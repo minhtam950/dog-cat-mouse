11ec72be826b9f0d6f06225e42ea7f65f16dacd2
Tam <trieuvan950@gmail.com>
2020-04-03 19:54:19 +0700

    Say Hi My Dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,14 @@
-function Dog(){
-	
+var chalk = require('chalk');
+function Dog(name){
+	this.name = name;
+	this.stomach = [];
 }
+Dog.prototype.eat = function(cat) {
+	// body...
+  this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function() {
+	// body...
+	console.log('Hi I am a Dog' + chalk.green(this.name));
+};
+module.exports = Dog;
